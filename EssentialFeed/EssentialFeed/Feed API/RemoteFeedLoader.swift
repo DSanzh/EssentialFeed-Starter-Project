f9d6b7ce5d4a7928043ef1d8b2dca1c264010617
Sanzhar <dauylov.sanzhar@gmail.com>
2025-02-27 17:19:21 +0500

    moved mapping logic into new FeedItemMapper

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,9 +39,10 @@ public class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case .success(let data, let response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map { $0.item }  ))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalideData))
                 }
             case .failiure:
@@ -51,18 +52,26 @@ public class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-private struct Item: Decodable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
-    
-    var item: FeedItem {
-        .init(id: id, description: description, location: location, imageURL: image)
+    private struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var item: FeedItem {
+            .init(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalideData
+        }
+        return try JSONDecoder().decode(Root.self, from: data).items.map { $0.item }
     }
 }
-
