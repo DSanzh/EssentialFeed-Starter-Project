300a12db7dad62b0566c81c249dbdd1d48df8484
Sanzhar <dauylov.sanzhar@gmail.com>
2025-03-11 18:58:33 +0500

    creates URLSession data task with URL on 'URLSessionHTTPClient.get(from: URL)'

---
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -0,0 +1,16 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Sanzhar Dauylov on 11.03.2025.
+//
+
+import Testing
+
+struct URLSessionHTTPClientTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
