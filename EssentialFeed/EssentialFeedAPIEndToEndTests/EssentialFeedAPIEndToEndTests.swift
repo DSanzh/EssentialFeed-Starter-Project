7e2317880016ef899f33c27e546bc9abce5797eb
Sanzhar <dauylov.sanzhar@gmail.com>
2025-03-13 18:26:01 +0500

    Add end to end test for getting and parsing correct amount of items from response

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Sanzhar Dauylov on 13.03.2025.
+//
+
+import Testing
+
+struct EssentialFeedAPIEndToEndTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
